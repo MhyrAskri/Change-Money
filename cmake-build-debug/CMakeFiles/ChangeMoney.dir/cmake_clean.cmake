a4eb8edface18487184f9c01c43a706b647370b7
MhyrAskri <askari.m1379@gmail.com>
2021-09-06 19:49:07 +0430

    change money (const)

---
+++ cmake-build-debug/CMakeFiles/ChangeMoney.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChangeMoney.dir/main.cpp.obj"
+  "ChangeMoney.exe"
+  "ChangeMoney.exe.manifest"
+  "ChangeMoney.pdb"
+  "libChangeMoney.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChangeMoney.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
